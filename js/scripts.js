f3da43bf70ca3828e93b9980a431ed3002397d00
Marko Ludlow <ludlow.marko@gmail.com>
2017-04-07 10:00:39 -0700

    create if statements for cost of pizza prototype

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,30 @@ function Pizza (name, topping, size) {
 this.name = name;
 this.topping = topping;
 this.size = size;
-
-var pizzaOutput = [];
 }
 
+Pizza.prototype.cost = function() {
+  if (this.topping === 0 && this.size === 0) {
+    return 5;
+  } else if (this.topping === 0 && this.size === 1) {
+    return 7;
+  } else if (this.topping === 0 && this.size === 2) {
+    return 9;
+  } else if (this.topping === 1 && this.size === 0) {
+    return 6;
+  } else if (this.topping === 1 && this.size === 1) {
+    return 8;
+  } else if (this.topping === 1 && this.size === 2) {
+    return 10;
+  } else if (this.topping === 2 && this.size === 0) {
+    return 5.50;
+  } else if (this.topping === 2 && this.size === 1) {
+    return 7.50;
+  } else if (this.topping === 2 && this.size === 2) {
+    return 9.50;
+  }
+}
+var pizzaOutput = [];
 
 //User-interface logic
 $(document).ready(function(){
