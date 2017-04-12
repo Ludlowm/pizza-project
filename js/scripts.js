7dc45916485a8a2513381c3b1e44c6cc773bfda1
Marko Ludlow <ludlow.marko@gmail.com>
2017-04-11 22:31:34 -0700

    remove forEach statement and edit syntax.

--- js/scripts.js
+++ js/scripts.js
@@ -3,34 +3,32 @@
 function Pizza (name, size, topping) {
 this.name = name;
 this.size = size;
-this.topping = topping;
+this.topping = [];
+this.cost = 0;
 }
-// function fullOrder (pizzaOutput) {
-//  this.pizzaOutput = [];
-//  }
 
 Pizza.prototype.cost = function() {
-  if (this.topping === 0 && this.size === 0) {
-    this.cost = 5;
-  } else if (this.topping === 0 && this.size === 1) {
-      this.cost = 7;
-  } else if (this.topping === 0 && this.size === 2) {
-      this.cost = 9;
-  } else if (this.topping === 1 && this.size === 0) {
-      this.cost = 6;
-  } else if (this.topping === 1 && this.size === 1) {
-      this.cost = 8;
-  } else if (this.topping === 1 && this.size === 2) {
-      this.cost = 10;
-  } else if (this.topping === 2 && this.size === 0) {
-      this.cost = 5.50;
-  } else if (this.topping === 2 && this.size === 1) {
-      this.cost = 7.50;
-  } else if (this.topping === 2 && this.size === 2) {
-      this.cost = 9.50;
+  if (this.topping === 0 && this.size === "small") {
+    this.cost += 5;
+  } else if (this.topping === 0 && this.size === "medium") {
+      this.cost += 7;
+  } else if (this.topping === 0 && this.size === "large") {
+      this.cost += 9;
+  } else if (this.topping === 1 && this.size === "small") {
+      this.cost += 6;
+  } else if (this.topping === 1 && this.size === "medium") {
+      this.cost += 8;
+  } else if (this.topping === 1 && this.size === "large") {
+      this.cost += 10;
+  } else if (this.topping === 2 && this.size === "small") {
+      this.cost += 5.50;
+  } else if (this.topping === 2 && this.size === "medium") {
+      this.cost += 7.50;
+  } else if (this.topping === 2 && this.size === "large") {
+      this.cost += 9.50;
   }
+  return this.cost;
 }
-var pizzaOutput = [];
 
 //User-interface logic
 $(document).ready(function(){
@@ -39,12 +37,9 @@ $(document).ready(function(){
     var userName = $("#name-input").val();
     var sizeInput = parseInt($("#size-select").val());
     var toppingInput = parseInt($("#topping-select").val());
-    var newPizza = new Pizza (userName, sizeInput, toppingInput);
-    var totalCost = 0;
-    pizzaOutput.push(newPizza);
-    pizzaOutput.forEach(function(pizza) {
-      totalCost += pizza.cost();
-       $(("ul#result").pizzaOutput());
+    newPizza.topping.push(toppingInput);
+    var newPizza = new Pizza(userName, sizeInput, toppingInput);
+       $("#result").append(userName);
+       newPizza.cost();
     });
   });
-});
